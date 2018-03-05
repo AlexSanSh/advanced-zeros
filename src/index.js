0394d655df3b67f6bcc038dde9180722eb7fd8cc
AlexSanSh <shpyra007@gmail.com>
2018-03-05 16:05:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ module.exports = function getZerosCount(number, base) {
 const factors = [];
 let num = 2;
 let countZeros = 0;
-const getFactors = (number) => {
+let count = 0;
+function getFactors(number) {
   if (number%num===0) {
     factors.push(num);
     number = number / num;
@@ -14,21 +15,16 @@ const getFactors = (number) => {
     }
   }
 }
-const getMainFactors = (arr) => {
-  let count = 0;
-  const maxItem = factors[factors.length-1];
-  for (let i = 0; i < factors.length; i++) {
-    if (factors[i]===maxItem) count ++;
-  }
-return [maxItem, count];
-}
 getFactors(base);
-const mainFactors = getMainFactors(factors);
-for (let i = number; i >=2; i--) {
-        for (let j = i; j>=2; j=j/mainFactors[0]) {
-          if (j%mainFactors[0]===0) countZeros++ ;
+const maxItem = factors[factors.length-1];
+  for (let i = 0; i < factors.length; i++) {
+    if (factors[i]===maxItem) count++;
+  };
+for (let i = number; i >=maxItem; i--) {
+        for (let j = i; j>=maxItem; j=j/maxItem) {
+          if (j%maxItem===0) countZeros++ ;
           else break;
         }
   }
-  return countZeros/mainFactors[1];
+  return countZeros/count;
 }
