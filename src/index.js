060055dd13dd95e21142bd266729c274a65dd824
AlexSanSh <shpyra007@gmail.com>
2018-03-06 21:44:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-// your implementation
-  
-var prime_num = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251];
-var p = [];
-var a = [];
-var base2 = base;
-var next_prime = 0;
-while (base2 > 1) {
-    while (base2 % prime_num[next_prime] != 0) {
-        next_prime++;
-    }
-    p.push (prime_num[next_prime]);
-    var pow = 0;
-    while (base2 % prime_num[next_prime] == 0) {
-        pow++;
-        base2 /= prime_num[next_prime];
-    }
-    a.push(pow);
-}
+const primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251];
+let factorial = number;
+let countSystem = base;
+const primes = [];
+ for (let i = 0; i < primeNums.length; i++) {
+   if (countSystem>1) {
+       while (countSystem%primeNums[i]===0) {
+         primes.push(primeNums[i]);
+         countSystem /= primeNums[i];
+     }
+   }
+ }
+const maxPrime = primes[primes.length-1];
+let pow = 0;
+  for (var i = 0; i < primes.length; i++) {
+    if (primes[i]===maxPrime) pow++;
+  };
+let zeroQuantity = 0;
+ while (factorial/maxPrime>0) {
+   zeroQuantity+=parseInt(factorial/maxPrime);
+   factorial = parseInt(factorial/maxPrime);
+ }
 
-var numbers = number;
-
-for (var i = 0; i < p.length; ++i) {
-    var c = 0;
-    var number_for_devide = number;
-
-    while (number_for_devide / p[i] > 0) {
-        c += Math.floor(number_for_devide/p[i]);
-        number_for_devide = Math.floor(number_for_devide/p[i]);
-    }
-    numbers = Math.floor(Math.min(numbers, c/a[i]));
-}
-
-return numbers;
+ return parseInt(zeroQuantity/pow);
 };
